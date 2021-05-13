a6666fd5e95038a975dd4c59d61531e61513bb30
aleafwolf <51769728+ALeafWolf@users.noreply.github.com>
2021-05-12 21:12:11 -0400

    change parameter toward to language/server option

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const Schema = mongoose.Schema;
 const CardSchema = new Schema({
     id: String,
     name: String,
+    nameEN: String,
     type: String,
     rarity: String,
     character: String,
@@ -46,10 +47,12 @@ const Card = mongoose.model("cards", CardSchema)
 const SkillSchema = new Schema({
     id: String,
     name: String,
+    nameEN: String,
     character: String,
     type: String,
     rarity: Array,
     description: String,
+    descriptionEN: String,
     nums: Array,
     function: String
 })
@@ -66,17 +69,22 @@ const CardPoolHistorySchema = new Schema({
     endDate: String,
     type: String,
     cards: Array,
-    bid:String,
+    bid: String,
     bv: String
 })
 const CardPoolHistory = mongoose.model(`card_pool_history`, CardPoolHistorySchema, `card_pool_history`)
 
-app.get("/api", (req, res) => {
-    res.status(200).send({"message": "Welcome to Tears of Themis API."})
+app.get("/", (req, res) => {
+    res.status(200).send({ "message": "Welcome to Tears of Themis API." })
 });
 
-// GET cards
-app.get("/api/cn/cards", (req, res) => {
+app.get("/api", (req, res) => {
+    res.status(200).send({ "message": "Welcome to Tears of Themis API." })
+});
+
+/*--------------------Card--------------------*/
+// GET cards in all language
+app.get("/api/cards", (req, res) => {
     Card.find().exec().then(
         (results) => {
             res.status(200).send(results);
@@ -84,18 +92,38 @@ app.get("/api/cn/cards", (req, res) => {
     ).catch(
         (err) => {
             console.log(err)
-            res.status(500).send("Error when getting cards from database.")
+            res.status(500).send({"message": "Error when getting cards from database."})
         }
     )
 });
 
-//GET one card
-app.get("/api/cn/card/:name", (req, res) => {
+//GET one card in all language
+app.get("/api/card/:name", (req, res) => {
+    let input = req.params.name;
+    Card.find({ $or: [{ name: input }, { nameEN: input }] }).exec().then(
+        (results) => {
+            if (results.length === 0) {
+                res.status(404).send({ "message": `${input} does not found` })
+            } else {
+                res.status(200).send(results[0]);
+            }
+        }
+    ).catch(
+        (err) => {
+            console.log(err)
+            res.status(500).send({"message": "Error when getting cards from database."})
+        }
+    )
+});
+
+//GET one card for certain language
+app.get("/api/:lang/card/:name", (req, res) => {
+    let lang = req.params.lang;
     let input = req.params.name;
     Card.find({ name: input }).exec().then(
         (results) => {
             if (results.length === 0) {
-                res.status(404).send({"message": `${input} does not found`})
+                res.status(404).send({ "message": `${input} does not found` })
             } else {
                 res.status(200).send(results);
             }
@@ -109,11 +137,13 @@ app.get("/api/cn/card/:name", (req, res) => {
 
 //GET one card: wrong format
 app.get("/api/cn/cards/:name", (req, res) => {
-    res.status(200).send({"message": `For find one card, use /card/:name`})
+    res.status(200).send({ "message": `For find one card, use /card/:name` })
 });
 
-//GET skills
-app.get("/api/cn/skills", (req, res) => {
+
+/*--------------------Skill--------------------*/
+//GET skills in all language
+app.get("/api/skills", (req, res) => {
     Skill.find().exec().then(
         (results) => {
             res.status(200).send(results);
@@ -121,20 +151,21 @@ app.get("/api/cn/skills", (req, res) => {
     ).catch(
         (err) => {
             console.log(err)
-            res.status(500).send("Error when getting skills from database.")
+            res.status(500).send({ "message": "Error when getting skills from database." })
         }
     )
 });
 
-//GET one skill
-app.get("/api/cn/skill/:name", (req, res) => {
+//GET one skill in all language
+app.get("/api/skill/:name", (req, res) => {
     let input = req.params.name;
-    Skill.find({ name: input }).exec().then(
+
+    Skill.find({ $or: [{ name: input }, { nameEN: input }] }).exec().then(
         (results) => {
             if (results.length === 0) {
-                res.status(404).send({"message": `${input} does not found`})
+                res.status(404).send({ "message": `Skill ${input} does not found` })
             } else {
-                res.status(200).send(results);
+                res.status(200).send(results[0]);
             }
         }
     ).catch(
@@ -145,10 +176,9 @@ app.get("/api/cn/skill/:name", (req, res) => {
 });
 
 //GET one skill: wrong format
-app.get("/api/cn/skills/:name", (req, res) => {
-    res.status(200).send({"message": `For find one skill, use /cards/:name`})
+app.get("/api/skills/:name", (req, res) => {
+    res.status(200).send({ "message": `For find one skill, use /cards/:name` })
 });
-
 
 // GET skill level up resourse
 app.get("/api/skillrss", (req, res) => {
@@ -159,13 +189,14 @@ app.get("/api/skillrss", (req, res) => {
     ).catch(
         (err) => {
             console.log(err)
-            res.status(500).send("Error when getting skill level up resourse from database.")
+            res.status(500).send({ "message": "Error when getting skill level up resourse from database." })
         }
     )
 });
 
-// GET card pool history
-app.get("/api/cn/poolhistory", (req, res) => {
+/*--------------------Vision History--------------------*/
+// GET rate up vision history for all servers
+app.get("/api/visionhistory", (req, res) => {
     CardPoolHistory.find().exec().then(
         (results) => {
             res.status(200).send(results);
@@ -173,7 +204,25 @@ app.get("/api/cn/poolhistory", (req, res) => {
     ).catch(
         (err) => {
             console.log(err)
-            res.status(500).send("Error when getting card pool history from database.")
+            res.status(500).send({ "message": "Error when getting card pool history from database." })
+        }
+    )
+});
+// GET rate up vision history for certain server
+app.get("/api/visionhistory/:server", (req, res) => {
+    let s = req.params.server
+    CardPoolHistory.find({ server: s }).exec().then(
+        (results) => {
+            if (results.length == 0) {
+                res.status(404).send({ "message": `No result find for vision history at server ${s}\nAvailable server paramters:\nCN as Chinese Mainland Server\nIN as International Server` });
+            } else {
+                res.status(200).send(results);
+            }
+        }
+    ).catch(
+        (err) => {
+            console.log(err)
+            res.status(500).send({ "message": "Error when getting card pool history from database." })
         }
     )
 });
